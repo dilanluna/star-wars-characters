4635eee9706b1b850b1e89086da06f5bb0fcb0c1
Dylan Luna <me@dilanluna.com>
2022-07-19 13:58:12 -0600

    ci(gh-pages): deploy app to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL || '/',
   plugins: [
     react(),
     checker({
