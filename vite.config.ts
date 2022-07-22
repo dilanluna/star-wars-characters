c835e4aefd38933f61d14339430952e6530c4889
Dylan Luna <me@dilanluna.com>
2022-07-22 12:04:37 -0600

    build: change output files name

--- vite.config.ts
+++ vite.config.ts
@@ -17,4 +17,29 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['src/setup-tests.ts'],
   },
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'static/js/[name].[hash].js',
+        chunkFileNames: 'static/js/[name].[hash].js',
+        assetFileNames: (assetInfo) => {
+          const filePattern = '[name].[hash].[ext]';
+
+          if (assetInfo.name.endsWith('.css')) {
+            return `static/css/${filePattern}`;
+          }
+
+          if (assetInfo.name.match(/\.(png|jpe?g|gif|svg)$/i)) {
+            return `static/img/${filePattern}`;
+          }
+
+          if (assetInfo.name.match(/\.(woff|woff2|eot|ttf|otf|svg)$/)) {
+            return `static/fonts/${filePattern}`;
+          }
+
+          return `static/assets/${filePattern}`;
+        },
+      },
+    },
+  },
 });
